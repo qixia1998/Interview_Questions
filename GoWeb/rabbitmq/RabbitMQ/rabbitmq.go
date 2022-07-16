50f525da86e494b6d100ce91549da2f17ba98882
qixia1998 <qixia717138552@gmail.com>
2022-07-16 10:43:56 +0800

    Updated rabbitmq_topic

--- GoWeb/rabbitmq/RabbitMQ/rabbitmq.go
+++ GoWeb/rabbitmq/RabbitMQ/rabbitmq.go
@@ -343,3 +343,111 @@ func (r *RabbitMQ) RecieveRouting() {
 	fmt.Println("退出请按 CTRL+C\n")
 	<-forever
 }
+
+// 话题模式
+// 创建RabbitMQ实例
+func NewRabbitMQTopic(exchangeName string, routingKey string) *RabbitMQ {
+	// 创建RabbitMQ实例
+	rabbitmq := NewRabbitMQ("", exchangeName, routingKey)
+	var err error
+	// 获取connection
+	rabbitmq.conn, err = amqp.Dial(rabbitmq.Mqurl)
+	rabbitmq.failOnErr(err, "failed to connect rabbitmq!")
+	// 获取channel
+	rabbitmq.channel, err = rabbitmq.conn.Channel()
+	rabbitmq.failOnErr(err, "failed to open a channel")
+	return rabbitmq
+}
+
+// 话题模式发送消息
+func (r *RabbitMQ) PublishTopic(message string) {
+	// 1. 尝试创建交换机
+	err := r.channel.ExchangeDeclare(
+		r.Exchange,
+		// 要改成direct
+		"topic",
+		true,
+		false,
+		false,
+		false,
+		nil,
+	)
+
+	r.failOnErr(err, "Failed to declare an exchange")
+
+	// 2.发送消息
+	err = r.channel.Publish(
+		r.Exchange,
+		// 要设置
+		r.Key,
+		false,
+		false,
+		amqp.Publishing{
+			ContentType: "text/plain",
+			Body:        []byte(message),
+		})
+
+}
+
+// 话题模式接收消息
+// 话题模式接收消息
+// 要注意key，规则
+// 其中"*"用于匹配一个单词，"#"用于匹配多个单词（可以是零个）
+func (r *RabbitMQ) RecieveTopic() {
+	// 1. 试探性创建交换机
+	err := r.channel.ExchangeDeclare(
+		r.Exchange,
+		// 交换机类型
+		"topic",
+		true,
+		false,
+		false,
+		false,
+		nil,
+	)
+
+	r.failOnErr(err, "Failed to declare an exchange")
+
+	// 2.试探性创建队列，这里注意队列名称不要写
+	q, err := r.channel.QueueDeclare(
+		"", // 随机产生队列名称
+		false,
+		false,
+		true,
+		false,
+		nil,
+	)
+	r.failOnErr(err, "Failed to declare a queue")
+
+	// 绑定队列到 exchange 中
+	err = r.channel.QueueBind(
+		q.Name,
+		// 需要绑定Key
+		r.Key,
+		r.Exchange,
+		false,
+		nil,
+	)
+
+	// 消费消息
+	messages, err := r.channel.Consume(
+		q.Name,
+		"",
+		true,
+		false,
+		false,
+		false,
+		nil,
+	)
+
+	forever := make(chan bool)
+
+	go func() {
+		for d := range messages {
+			log.Printf("Received a message: %s", d.Body)
+		}
+	}()
+
+	fmt.Println("退出请按 CTRL+C\n")
+	<-forever
+}
