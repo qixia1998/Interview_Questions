09c189e1974ab062921f825f764b3e418afb5b0f
qixia1998 <qixia717138552@gmail.com>
2022-08-05 11:25:17 +0800

    Updated

--- GoWeb/go_redis/main.go
+++ GoWeb/go_redis/main.go
@@ -57,6 +57,93 @@ func V8Example() {
 	// key2 does not exist
 }
 
+// set/get
+func redisExample() {
+	ctx := context.Background()
+	if err := initClient(); err != nil {
+		return
+	}
+
+	err := rdb.Set(ctx, "score", 100, 0).Err()
+	if err != nil {
+		fmt.Printf("set score failed, err:%v\n", err)
+		return
+	}
+
+	val, err := rdb.Get(ctx, "score").Result()
+	if err != nil {
+		fmt.Printf("get score failed, err:%v\n", err)
+		return
+	}
+	fmt.Println("score", val)
+
+	val2, err := rdb.Get(ctx, "name").Result()
+	if err == redis.Nil {
+		fmt.Println("name does not exist")
+	} else if err != nil {
+		fmt.Printf("get name failed, err:%v\n", err)
+		return
+	} else {
+		fmt.Println("name", val2)
+	}
+}
+
+// zset
+func redisExample2() {
+	ctx := context.Background()
+	if err := initClient(); err != nil {
+		return
+	}
+
+	zsetKey := "language_rank"
+	languages := []redis.Z{
+		redis.Z{Score: 90.0, Member: "Golang"},
+		redis.Z{Score: 98.0, Member: "Java"},
+		redis.Z{Score: 95.0, Member: "Python"},
+		redis.Z{Score: 97.0, Member: "JavaScript"},
+		redis.Z{Score: 99.0, Member: "C/C++"},
+	}
+	// ZADD
+	num, err := rdb.ZAdd(ctx, zsetKey, languages...).Result()
+	if err != nil {
+		fmt.Printf("zadd failed, err:%v\n", err)
+		return
+	}
+	fmt.Printf("zadd %d succ.\n", num)
+
+	// 把Golang的分数加10
+	newScore, err := rdb.ZIncrBy(ctx, zsetKey, 10.0, "Golang").Result()
+	if err != nil {
+		fmt.Printf("zincrby failed, err:%v\n", err)
+		return
+	}
+	fmt.Printf("Golang's score is %f now.\n", newScore)
+
+	// 取分数最高的3个
+	ret, err := rdb.ZRevRangeWithScores(ctx, zsetKey, 0, 2).Result()
+	if err != nil {
+		fmt.Printf("zrevrange failed, err:%v\n", err)
+		return
+	}
+	for _, z := range ret {
+		fmt.Println(z.Member, z.Score)
+	}
+
+	// 取95~100分的
+	op := &redis.ZRangeBy{
+		Min: "95",
+		Max: "100",
+	}
+	ret, err = rdb.ZRangeByScoreWithScores(ctx, zsetKey, op).Result()
+	if err != nil {
+		fmt.Printf("zrangebyscore failed, err:%v\n", err)
+		return
+	}
+	for _, z := range ret {
+		fmt.Println(z.Member, z.Score)
+	}
+}
+
 // 连接Redis哨兵模式
 //func initClient() (err error) {
 //	rdb := redis.NewFailoverClient(&redis.FailoverOptions{
@@ -81,3 +168,13 @@ func V8Example() {
 //	}
 //	return nil
 //}
+
+func main() {
+	if err := initClient(); err != nil {
+		fmt.Printf("init redis client failed, err:%v\n", err)
+		return
+	}
+	fmt.Println("connect redis success...")
+	// 程序退出时释放资源
+	defer rdb.Close()
+}
