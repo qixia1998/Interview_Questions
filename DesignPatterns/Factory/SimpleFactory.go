241c2ebc8ccf0f9f7bbf171fcf536939246ad42a
qixia1998 <qixia717138552@gmail.com>
2022-10-01 23:39:17 +0800

    Updated simpleFactory

--- DesignPatterns/Factory/SimpleFactory.go
+++ DesignPatterns/Factory/SimpleFactory.go
@@ -2,17 +2,21 @@ package Factory
 
 import "fmt"
 
-type Person struct {
+type Person interface {
+	Greet()
+}
+
+type person struct {
 	Name string
 	Age  int
 }
 
-func (p Person) Greet() {
+func (p person) Greet() {
 	fmt.Printf("Hi! My name is %s", p.Name)
 }
 
-func NewPerson(name string, age int) *Person {
-	return &Person{
+func NewPerson(name string, age int) Person {
+	return person{
 		Name: name,
 		Age:  age,
 	}
