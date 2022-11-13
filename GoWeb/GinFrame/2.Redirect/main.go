0e0559443faf2b3c626d094ca33669a0cc583967
qixia1998 <qixia717138552@gmail.com>
2022-11-13 23:12:56 +0800

    Updated redirect

--- GoWeb/GinFrame/2.Redirect/main.go
+++ GoWeb/GinFrame/2.Redirect/main.go
@@ -13,5 +13,18 @@ func main() {
 		url := "http://www.baidu.com"
 		c.Redirect(http.StatusMovedPermanently, url)
 	})
+
+	// 路由重定向: 重定向到具体路由
+	r.GET("/redirect2", func(c *gin.Context) {
+		c.Request.URL.Path = "/TestRedirect"
+		r.HandleContext(c)
+	})
+	// 路由：127.0.0.1:9090/TestRedirect
+	r.GET("/TestRedirect", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{
+			"code": http.StatusOK,
+			"msg":  "响应成功",
+		})
+	})
 	r.Run(":9090")
 }
