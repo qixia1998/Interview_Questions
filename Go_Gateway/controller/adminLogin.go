829e18715600218a562bc9381e128ab3489ed58d
qixia1998 <qixia717138552@gmail.com>
2023-06-07 23:44:49 +0800

    ADD Admin

--- Go_Gateway/controller/adminLogin.go
+++ Go_Gateway/controller/adminLogin.go
@@ -1,9 +1,15 @@
 package controller
 
 import (
+	"encoding/json"
+	"github.com/e421083458/golang_common/lib"
+	"github.com/gin-gonic/contrib/sessions"
 	"github.com/gin-gonic/gin"
+	"github.com/qixia1998/Interview_Questions/Go_Gateway/dao"
 	"github.com/qixia1998/Interview_Questions/Go_Gateway/dto"
 	"github.com/qixia1998/Interview_Questions/Go_Gateway/middleware"
+	"github.com/qixia1998/Interview_Questions/Go_Gateway/public"
+	"time"
 )
 
 type AdminLoginController struct{}
@@ -11,6 +17,7 @@ type AdminLoginController struct{}
 func AdminLoginRegister(group *gin.RouterGroup) {
 	adminLogin := &AdminLoginController{}
 	group.POST("/login", adminLogin.AdminLogin)
+	group.GET("/logout", adminLogin.AdminLoginOut)
 }
 
 // AdminLogin godoc
@@ -23,12 +30,59 @@ func AdminLoginRegister(group *gin.RouterGroup) {
 // @Param body body dto.AdminLoginInput true "body"
 // @Success 200 {object} middleware.Response{data=dto.AdminLoginOutput} "success"
 // @Router /admin_login/login [post]
-func (adminLogin *AdminLoginController) AdminLogin(c *gin.Context) {
+func (adminlogin *AdminLoginController) AdminLogin(c *gin.Context) {
 	params := &dto.AdminLoginInput{}
 	if err := params.BindValidParam(c); err != nil {
-		middleware.ResponseError(c, 1001, err)
+		middleware.ResponseError(c, 2000, err)
 		return
 	}
-	out := &dto.AdminLoginOutput{Token: params.UserName}
+
+	//1. params.UserName 取得管理员信息 admininfo
+	//2. admininfo.salt + params.Password sha256 => saltPassword
+	//3. saltPassword==admininfo.password
+	tx, err := lib.GetGormPool("default")
+	if err != nil {
+		middleware.ResponseError(c, 2001, err)
+		return
+	}
+	admin := &dao.Admin{}
+	admin, err = admin.LoginCheck(c, tx, params)
+	if err != nil {
+		middleware.ResponseError(c, 2002, err)
+		return
+	}
+
+	//设置session
+	sessInfo := &dto.AdminSessionInfo{
+		ID:        admin.Id,
+		UserName:  admin.UserName,
+		LoginTime: time.Now(),
+	}
+	sessBts, err := json.Marshal(sessInfo)
+	if err != nil {
+		middleware.ResponseError(c, 2003, err)
+		return
+	}
+	sess := sessions.Default(c)
+	sess.Set(public.AdminSessionInfoKey, string(sessBts))
+	sess.Save()
+
+	out := &dto.AdminLoginOutput{Token: admin.UserName}
 	middleware.ResponseSuccess(c, out)
+}
+
+// AdminLogin godoc
+// @Summary 管理员退出
+// @Description 管理员退出
+// @Tags 管理员接口
+// @ID /admin_login/logout
+// @Accept  json
+// @Produce  json
+// @Success 200 {object} middleware.Response{data=string} "success"
+// @Router /admin_login/logout [get]
+func (adminlogin *AdminLoginController) AdminLoginOut(c *gin.Context) {
+	sess := sessions.Default(c)
+	sess.Delete(public.AdminSessionInfoKey)
+	sess.Save()
+	middleware.ResponseSuccess(c, "")
 }
